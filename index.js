b72e37a211be51a01a74dae05dc444f1734c9b4c
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-18 13:35:52 -0400

    commit tree 0262fd649b719f165997412fc9dc1298c7c11447

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+
+function createFareMultiplier(int) {
+  return () => {
+   return int * int;
+  }
+};
+
+const fareDoubler = (int) => {
+  let multiplied = createFareMultiplier(int);
+  let answer = (multiplied() / int)
+  return answer * 2;
+}
+
+function fareTripler(int) {
+  return int * 3;
+}
+
+function selectDifferentDrivers(drivers, returnTheseDrivers) {
+  return returnTheseDrivers(drivers);
+}
